90743c7ab3d9edf10cfd20135c42c140e2184a48
Hasbunallah Mubarak Abiola <82613627+Hasbunallahmubarak@users.noreply.github.com>
2023-11-11 20:09:18 +0100

    Add files via upload

---
+++ js/hmabiola.js
@@ -0,0 +1,8 @@
+function openNav() {
+	document.getElementById('overlay').style.display = "block";
+	document.getElementById('nav').style.display = "block";
+}
+function closeNav() {
+	document.getElementById('overlay').style.display = "none";
+	document.getElementById('nav').style.display = "none";
+}
